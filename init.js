87bb608198d4942b660d8bc09e3bd2937bb4c134
Kaiyin Zhong <kindlychung@gmail.com>
2018-12-03 13:59:09 +0100

    first

---
+++ init.js
@@ -0,0 +1,8 @@
+rs.initiate({
+    _id: "rs0",
+    members: [
+        { _id: 0, host: "localhost:27017" },
+        { _id: 1, host: "localhost:27018" },
+        { _id: 2, host: "localhost:27019" }
+    ]
+})
